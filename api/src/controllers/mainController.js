b8bde44600c81f7ce6dc766547c02dc853843955
ramiro-e <ramiroestevez96@gmail.com>
2022-07-02 12:16:59 -0300

    Preliminary main routes and controllers

--- api/src/controllers/mainController.js
+++ api/src/controllers/mainController.js
@@ -1,6 +1,120 @@
+const db = require('../database/models');
+const Transaction = db.Transaction
+const User = db.User
+const Account = db.Account
+const { Op } = require("sequelize");
+
 const mainControllers = {
-    index: (req, res) =>{
-        
+    getAccouts:(req, res) =>{
+        Account.findAll({
+            where:{accountId: req.accountId}
+        })
+        .then((account)=>{
+            res.send(account)
+        })
+    },
+    getAccountBalance: (req, res) =>{
+        Transaction.findAll({
+            where:{accountId: req.accountId},
+            include: [{
+                model: Category
+            },{
+                model: Method
+            }]
+        })
+        .then((transactions) =>{ 
+            let balance = transactions.map((transaction) =>{ return transaction.amount; }).reduce((a,b)=> a + b);
+            return balance
+        })
+        .then((balance)=>{
+            res.send(balance)
+        })
+    },
+    getAllTransactions: (req, res) =>{
+        Account.findAll({
+            where:{userId: req.userId}
+        })
+        .then((accounts) =>{
+            let accountIdArray = accounts.map((account) =>{ return account.id; })
+            return Transaction.findAll({
+                where:{accountId: {[Op.or]: accountIdArray}},
+            })
+        })
+        .then((lastTransactions)=>{
+            res.send(lastTransactions)
+        })
+    },
+    getLastTransactions: (req, res) =>{
+        Account.findAll({
+            where:{userId: req.userId}
+        })
+        .then((accounts) =>{
+            let accountIdArray = accounts.map((account) =>{ return account.id; })
+            return Transaction.findAll({
+                where:{accountId: {[Op.or]: accountIdArray}},
+                limit: 10
+            })
+        })
+        .then((lastTransactions)=>{
+            res.send(lastTransactions)
+        })
+    },
+    getTransactionById: (req, res) =>{
+        Transaction.findByPk(req.transactionId,{
+            include: [{
+                model: Category
+            },{
+                model: Method
+            }]
+        })
+        .then((lastTransactions)=>{
+            res.send(lastTransactions)
+        })
+    },
+    newTransaction: (req, res) =>{
+        Transaction.create({
+            accountId: req.accountId,
+            datetime: req.datetime,
+            name: req.name,
+            categoryId: req.categoryId,
+            description: req.description,
+            amount: req.amount,
+            depOrWit: req.depOrWit,
+            methodId: req.methodId
+        })
+        .then(()=>{
+            res.send(200)
+        })
+    },
+    editTransaction: (req, res) =>{
+        Transaction.update({
+            accountId: req.accountId,
+            datetime: req.datetime,
+            name: req.name,
+            categoryId: req.categoryId,
+            description: req.description,
+            amount: req.amount,
+            methodId: req.methodId
+        },{
+            where:{
+                id: req.transactionId
+            }
+        })
+        .then(()=>{
+            res.send(200)
+        })
+    },
+    deleteTransaction: (req, res) =>{
+        Transaction.destroy({
+            where:{
+                id: req.transactionId
+            }
+        })
+        .then(()=>{
+            res.send(200)
+        })
     }
+
+
 }
 module.exports = mainControllers;
--- api/src/routers/mainRouter.js
+++ api/src/routers/mainRouter.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
-router.get('/', mainController.index);
+router.post('/getAccouts', mainController.getAccouts);
+router.post('/getAccountBalance', mainController.getAccountBalance);
+router.post('/getAllTransactions', mainController.getAllTransactions);
+router.post('/getLastTransactions', mainController.getLastTransactions);
+router.post('/getTransactionById', mainController.getTransactionById);
+router.post('/newTransaction', mainController.newTransaction);
+router.post('/editTransaction', mainController.editTransaction);
+router.post('/deleteTransaction', mainController.deleteTransaction);
 
 module.exports = router;
