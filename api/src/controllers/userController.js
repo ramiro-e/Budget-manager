1df9c610347e947763af2862199a75fecb48c10b
ramiro-e <ramiroestevez96@gmail.com>
2022-07-04 17:15:23 -0300

    Preliminary user routes and controllers

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,6 +1,55 @@
 const userControllers = {
-    index: (req, res) =>{
-        console.log("hola")
-    }
+    login: (req, res) => {
+        let validation = validationResult(req);             //return res.send(errors.mapped());
+        let errors = validation.errors;
+        console.log(errors);
+        if(validation.isEmpty()){
+            Users.findOne({
+                where:{email:req.body.email}
+            })                                              //return res.send(userLogin);
+            .then((user) => {
+                delete user.password;
+                jwt.sign({user}, 'secretkey', {expiresIn: '1h'}, (err, token) => {
+                    res.json({token});
+                });
+                return res.redirect('/');
+            })
+            .catch(error => console.log(error))
+        }else{
+            res.render(path.resolve(__dirname, '../views/user/login'),{
+                title: "Ingresar | Vigilancia Argentina",
+                errors
+            })
+        }
+    },
+    logout: (req,res) =>{
+        req.session.destroy();
+        res.clearCookie('email');
+        res.redirect('/')
+    },
+    register:  (req, res) => {
+        let validation = validationResult(req);
+        let errors = validation.errors;
+        if (validation.isEmpty()) {
+            Users.create({
+                email: req.email,
+                firstName: req.firstName,
+                lastName: req.lastName,
+                password: bcrypt.hashSync(req.body.password, 10),
+            })
+            .then(() => {
+                return res.render(path.resolve(__dirname, '../views/user/registerMessage'),{
+                    title: "¡Registro Exitoso! | Vigilancia Argentina"
+                });
+            })
+            .catch(error => res.send(error));
+        } else {
+            console.log(errors)
+            return res.render(path.resolve(__dirname, '../views/user/register'),{
+                title: "Registrarse | Vigilancia Argentina",
+                errors
+            });
+        }
+    },
 }
 module.exports = userControllers;
--- api/src/routers/userRouter.js
+++ api/src/routers/userRouter.js
@@ -1,7 +1,94 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const db = require('../database/models');
+const User = db.User
+const authorize = require('../middlewares/auth')
+const jwt = require('jsonwebtoken')
 
-router.get('/', userController.index);
+
+const validationLogin = (req, res, next) => {
+    User.findOne(
+        {where: {email: req.email}
+    })
+    .then(user => {
+        if (!user) {
+            res.send(404);
+        } else if(bcrypt.compareSync(req.password, user.dataValues.password)){
+            res.send(200);
+            next()
+        }else{
+            res.send(403);
+        }
+    })
+}
+
+const validateEmail = (email) => {
+    if(!email.toLowerCase().match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){ return false}
+    if(User.findOne(
+        {where: {email: req.email}
+    })){ return false}
+};
+
+const validatePassword = (password) => {
+    if(!password.match(/^(?=.*[a-z])(?=.{8,100})(?=.*\d).{8,100}$/)){   return false}
+};
+
+const ValidationRegister = (req, res, next) => {
+    if(!typeof req.firstName == 'string' || !req.firstName.length >= 1){
+        res.send(403);
+        return 
+    }else if(typeof req.lastName != 'string' || !req.lastName.length >= 1){
+        res.send(403);
+        return
+    }else if(typeof req.email != 'string' || !validateEmail(req.email)){
+        res.send(403);
+        return
+    }else if(typeof req.password != 'string' || !validatePassword(req.password)){
+        res.send(403);
+        return
+    }else{
+        next();
+    }
+};
+
+
+router.post('/login', validateEmail ,userController.login);
+router.post('/logout', userController.logout);
+
+router.post('/register', validationLogin ,userController.register);
+
+
+
+
+// router.get("/tokeninfo", (req , res) => {
+//     const user = {
+//         id: 1,
+//         nombre : "Henry",
+//         email: "henry@email.com"
+//     }
+
+//     jwt.sign({user}, 'secretkey', {expiresIn: '1h'}, (err, token) => {
+//         res.json({
+//             token
+//         });
+//     });
+    
+
+// });
+
+// router.post("/tokencheck", authorize, (req , res) => {
+//     console.log('tokencheck completed')    
+
+// });
+
+
+// res.json({
+//     mensaje: "Post fue creado",
+//     authData
+// });
+
 
 module.exports = router;
