0598e14e42e85fd2b4bf660076f49efe7e2d0001
ramiro-e <ramiroestevez96@gmail.com>
2022-07-17 20:19:19 +0100

    Home subcomponent implementation

--- frontend/src/components/homepage/Home.js
+++ frontend/src/components/homepage/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import AccountHeader from './partials/AccountHeader';
+import AccountCards from './partials/AccountCards';
+import LastTransactions from './partials/LastTransactions';
+import NewTransaction from './partials/NewTransaction';
+import TransactionPreviewEdit from './partials/TransactionPreviewEdit';
+
+
 function Home(){
 
     return(
         <React.Fragment>
+            <main>    
+                <section className="AccountContainer">
+                    <AccountHeader/>
+                    <AccountCards accountsCards={accountsCards}/>
+                </section>
+                <section className="TransactionContainer">
+                    <div className="row">                
+                        <div className="lastTransactionList col-md-6">
+                            <LastTransactions transactions={lastTransactions}/>
+                        </div>
+                        <div className="lastTransactionPreviewEdit col-md-6">
+
+                        </div>
+                    </div>
+                </section>
+            </main>
 
         </React.Fragment>
     )
