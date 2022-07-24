658d7eba5512f9ef89b7a3324027e967f10a6606
ramiro-e <ramiroestevez96@gmail.com>
2022-07-24 20:19:19 +0100

    Finished auth middleware

--- api/src/middlewares/auth.js
+++ api/src/middlewares/auth.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken')
 
 
 module.exports = async (req,res,next) =>{
+
     let bearerHeader =  req.headers['authorization'];
     let bearerToken 
     if(typeof bearerHeader !== 'undefined'){
@@ -14,11 +15,19 @@ module.exports = async (req,res,next) =>{
         res.sendStatus(403);
     }
 
+    // let tokensito = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoxLCJlbWFpbCI6InJhbWlyb2VzdGV2ZXo5NkBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJSYW1pcm8iLCJsYXN0TmFtZSI6IkVzdGV2ZXoiLCJwYXNzd29yZCI6IiQyYSQxMCRxVjNObWlLdzRDbkgwcUIwSWlHc3hlR0JZLzAwUzYzR1AzRWY5Y2RScG15UkQ0SUxUSGU5aSJ9LCJpYXQiOjE2NTkyMjY0NDIsImV4cCI6MTY1OTIzMDA0Mn0.y1Vq68RwnPQS53jCT1tFzgIpoTO8WH7fJszjFDmKkk8"
+
+    // console.log(typeof tokensito)
+    // console.log(bearerToken)
+
     jwt.verify(bearerToken, 'secretkey', (error, authData) => {
+
         if(error){
             res.sendStatus(403);
         }else{
-            req['userData'] = authData
+            console.log("success")
+            delete(authData.payload.password)
+            req['userData'] = authData.payload
             next()
         }
     });
