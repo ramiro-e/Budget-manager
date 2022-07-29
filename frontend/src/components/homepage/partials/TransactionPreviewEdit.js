8f16b62e8bb1db9b45f9286bfdac150249f26ca6
ramiro-e <ramiroestevez96@gmail.com>
2022-07-29 20:19:19 +0100

    implement api transaction create update delete

--- frontend/src/components/homepage/partials/TransactionPreviewEdit.js
+++ frontend/src/components/homepage/partials/TransactionPreviewEdit.js
@@ -6,12 +6,44 @@ import { Trash3 , PencilSquare, Save, XLg} from 'react-bootstrap-icons';
 
 
 
-function TransactionPreviewEdit({transaction, editState, newState, cancelMethod, editMethod}){
+function TransactionPreviewEdit({transaction, accounts, categories, methods, editState, newState, cancelMethod, editMethod, editTransaction, newTransaction, deleteTransaction}){
 
+
+    function getFormData(){
+        let accountId = document.getElementById('account').value
+        let name = document.getElementById('name').value
+        let categoryId = document.getElementById('category').value
+        let description = document.getElementById('description').value
+        let methodId = document.getElementById('method').value
+        let depOrWit = document.getElementById('depOrWit').value
+        let amount = document.getElementById('amount').value
+        if(depOrWit == 2){
+            amount = -Math.abs(amount)
+        }
+        return {accountId, name, categoryId, description, methodId, amount}   
+    }
+
+    function handleSubmit(){
+        let formData = {}
+        if(transaction.id){
+            formData = getFormData();
+            formData['id'] = transaction.id
+            editTransaction(formData)
+        }else{
+            formData = getFormData();
+            newTransaction(formData)
+        }
+    }
+
+
+    function handleDelete(){
+        deleteTransaction({id: transaction.id})
+    }
 
     if (transaction.amount < 0){
         transaction['depOrWit'] = 2
     }
+
 
     return (
         <React.Fragment>
@@ -73,11 +105,11 @@ function TransactionPreviewEdit({transaction, editState, newState, cancelMethod,
 
                     {editState || newState ?
                     <div className="d-flex justify-content-end mt-2">
-                        <Button variant="primary" className="rounded shadow-none p-1 px-2 d-flex align-items-center" > <Save className="me-2"/> Guardar</Button>
+                        <Button variant="primary" className="rounded shadow-none p-1 px-2 d-flex align-items-center"  onClick={handleSubmit}> <Save className="me-2"/> Guardar</Button>
                     </div>
                     :
                     <div className="d-flex justify-content-between mt-2">
-                        <Button variant="danger" className="rounded shadow-none p-1 px-2 me-2 d-flex align-items-center" ><Trash3 className="me-2"/>  Eliminar</Button>
+                        <Button variant="danger" className="rounded shadow-none p-1 px-2 me-2 d-flex align-items-center"  onClick={handleDelete}><Trash3 className="me-2"/>  Eliminar</Button>
                         <Button variant="primary" className="rounded shadow-none p-1 px-2 d-flex align-items-center"  onClick={editMethod}><PencilSquare className="me-2"/>Editar</Button>
                     </div>
                     }
